e7b1919d826cde473d8b17d0be289eab561c116c
RO pretypographic <a.pesvi@yandex.ru>
2023-09-19 09:25:12 +0300

    4o6a8utb kpam-tect cep8epa

--- backend/routes/router.js
+++ backend/routes/router.js
@@ -4,6 +4,12 @@ const cardsRouter = require('./cards');
 
 const NotFoundError = require('../errors/not-found-err');
 
+router.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
+
 router.use(usersRouter);
 router.use(cardsRouter);
 router.use('*', (req, res, next) => next(new NotFoundError('invalid address')));
